627c993832ae7209a6603d555bf9b0666a8ff495
Hadi Zamani <h.zamani@Hadi-iMac.local>
2016-12-24 15:22:42 +0330

    - Change Libs structure

---
+++ MemorizeItForever/BaseLocalDataAccess.framework/Headers/BaseLocalDataAccess.h
@@ -0,0 +1,19 @@
+//
+//  BaseLocalDataAccess.h
+//  BaseLocalDataAccess
+//
+//  Created by Hadi Zamani on 10/14/16.
+//  Copyright © 2016 SomeSimpleSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BaseLocalDataAccess.
+FOUNDATION_EXPORT double BaseLocalDataAccessVersionNumber;
+
+//! Project version string for BaseLocalDataAccess.
+FOUNDATION_EXPORT const unsigned char BaseLocalDataAccessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseLocalDataAccess/PublicHeader.h>
+
+
