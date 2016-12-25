05fd2bfe9f330e461111e20beaa85f32c66962d6
hadiz <hadi.zamani@outlook.com>
2016-12-25 23:16:32 +0330

    - Restructure Libs

--- MemorizeItForeverCore/MemorizeItForeverCoreTests/Helper/MockGenericDataAccess.swift
+++ MemorizeItForeverCore/MemorizeItForeverCoreTests/Helper/MockGenericDataAccess.swift
@@ -11,5 +11,34 @@ import CoreData
 
 
 class MockGenericDataAccess<TEntity>: GenericDataAccess<TEntity> where TEntity: EntityProtocol, TEntity: AnyObject, TEntity: NSFetchRequestResult {
+   
+//    public func fetchEntity(predicate: PredicateProtocol?, sort: SortProtocol?, fetchLimit: Int?) throws -> [TEntity] {
+//        return []
+//    }
+
+    typealias T = TEntity
     
+    required init(context: ManagedObjectContextProtocol) {
+        super.init(context: context)
+    }
+    
+//    override func createNewInstance() throws -> TEntity {
+//        return NSManagedObject() as! TEntity
+//    }
+//    
+//    override func saveEntity(_ entity: TEntity) throws {
+//        
+//    }
+//    
+    func fetchEntity(withId id: UUID) throws -> TEntity? {
+        return nil
+    }
+    
+//    func fetchEntityCount(predicate: PredicateProtocol?) throws -> Int {
+//        return 0
+//    }
+//    
+//    func deleteEntity(_ entity: TEntity) throws {
+//        
+//    }
 }
