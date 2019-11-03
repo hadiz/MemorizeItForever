cfb5eee32b5697e097194d2f20824e7e8e9c18cb
hadiz <hadi.zamani@outlook.com>
2019-11-03 23:46:53 +0100

    -Fix the bundle identifier

---
+++ MemorizeItForever/MemorizeItForever/General/MemorizeItDataSource/MemorizeItTableDataSourceProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  MemorizeItTableDataSource.swift
+//  MemorizeItForever
+//
+//  Created by Hadi Zamani on 5/25/16.
+//  Copyright © 2016 SomeSimpleSolution. All rights reserved.
+//
+
+import UIKit
+import MemorizeItForeverCore
+
+protocol MemorizeItTableDataSourceProtocol: UITableViewDataSource, UITableViewDelegate {
+    func setModels(_ models: [MemorizeItModelProtocol])
+}
+
